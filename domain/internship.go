e70dd9ed70530fb4881f611909cce17107634104
kobulwidodo <fadhilhan01@gmail.com>
2022-03-25 00:02:01 +0700

    Add some struct and interface

--- domain/internship.go
+++ domain/internship.go
@@ -8,5 +8,29 @@ type Internship struct {
 	Description string
 	Company     string
 	IsPaid      bool
+	UserId      uint `json:"-"`
+}
+
+type InternshipRepository interface {
+	Create(internship Internship) error
+	FindOne(id uint) (Internship, error)
+	FindAll() ([]Internship, error)
+}
+
+type InternshipUseCase interface {
+	CreateInternship(input *CreateInternship) error
+	GetInternshipById(input *FindInternship) (Internship, error)
+	GetInternships() ([]Internship, error)
+}
+
+type CreateInternship struct {
+	Position    string `binding:"required"`
+	Description string `binding:"required"`
+	Company     string `binding:"required"`
+	IsPaid      *bool  `json:"is_paid" binding:"required"`
 	UserId      uint
+}
+
+type FindInternship struct {
+	Id uint `uri:"id" binding:"required"`
 }
