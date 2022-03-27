517ee76faff8c41bc63fc27f654ce682e0ea1068
kobulwidodo <fadhilhan01@gmail.com>
2022-03-27 20:09:13 +0700

    Add interface and struct

--- domain/internship_applicant.go
+++ domain/internship_applicant.go
@@ -13,3 +13,24 @@ type InternshipApplicant struct {
 	InternshipId uint
 	Internship   Internship
 }
+
+type InternshipApplicantPostgresRepository interface {
+	Create(internshipApplicant InternshipApplicant) error
+}
+
+type InternshipAppilcantUseCase interface {
+	Apply(input *ApplyInternship) error
+}
+
+type ApplyInternship struct {
+	Age          int    `binding:"requried"`
+	College      string `binding:"requried"`
+	Major        string `binding:"requried"`
+	CvLink       string `binding:"requried"`
+	UserId       uint
+	InternshipId uint
+}
+
+type FindInternshipUri struct {
+	InternshipId uint `uri:"internship_id" binding:"required"`
+}
