6d95bc2174b7aa8c62862641a3685dbf0357da30
kobulwidodo <fadhilhan01@gmail.com>
2022-03-27 20:10:13 +0700

    Rename file, add postgresql

---
+++ internship/repository/postgresql/internship_postgresql.go
@@ -0,0 +1,54 @@
+package postgresql
+
+import (
+	"talentgrow-backend/domain"
+
+	"gorm.io/gorm"
+)
+
+type InternshipRepository struct {
+	db *gorm.DB
+}
+
+func NewInternshipRepository(db *gorm.DB) domain.InternshipRepository {
+	return &InternshipRepository{db: db}
+}
+
+func (r *InternshipRepository) Create(internship domain.Internship) error {
+	if err := r.db.Create(&internship).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *InternshipRepository) FindOne(id uint) (domain.Internship, error) {
+	var internship domain.Internship
+	if err := r.db.Where("id = ?", id).First(&internship).Error; err != nil {
+		return internship, err
+	}
+
+	return internship, nil
+}
+
+func (r *InternshipRepository) FindAll() ([]domain.Internship, error) {
+	var internship []domain.Internship
+	if err := r.db.Find(&internship).Error; err != nil {
+		return internship, err
+	}
+	return internship, nil
+}
+
+func (r *InternshipRepository) Update(internship domain.Internship) error {
+	if err := r.db.Save(&internship).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *InternshipRepository) Delete(internship domain.Internship) error {
+	if err := r.db.Delete(&internship).Error; err != nil {
+		return err
+	}
+	return nil
+}
