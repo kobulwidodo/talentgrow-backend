eacf0c733bd12a08cb08f4d39e635c9c32114156
kobulwidodo <fadhilhan01@gmail.com>
2022-04-22 13:27:16 +0700

    feat: add some interface and struct

--- domain/event.go
+++ domain/event.go
@@ -8,9 +8,51 @@ import (
 
 type Event struct {
 	gorm.Model
-	Title       string
-	Description string
-	Type        string
-	Date        time.Time
-	UserId      uint
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Type        string `json:"type"`
+	Date        time.Time `json:"date"`
+	UserId      uint `json:"-"`
+}
+
+type EventRepository interface {
+	Create(event Event) error
+	FindAll() ([]Event, error)
+	FindOne(id uint) (Event, error)
+	FindByType(key string) ([]Event, error)
+	Update(event Event) error
+	Delete(event Event) error
+}
+
+type EventUseCase interface {
+	Create(input *CreateEventDto) error
+	GetAll() ([]Event, error)
+	GetByType(key string) ([]Event, error)
+	GetById(input *FindEventUri) (Event, error)
+	Update(input *UpdateEventDto) error
+	Delete(id uint) error
+}
+
+type CreateEventDto struct {
+	Title       string `binding:"required"`
+	Description string `binding:"required"`
+	Type        string `binding:"required"`
+	Date        string `binding:"required"`
+	UserId uint
+}
+
+type FindEventUri struct {
+	Id uint `uri:"id" binding:"required"`
+}
+
+type FindEventsType struct {
+	Type string `uri:"type" binding:"required"`
+}
+
+type UpdateEventDto struct {
+	Id uint
+	Title       string `binding:"required"`
+	Description string `binding:"required"`
+	Type        string `binding:"required"`
+	Date        time.Time `binding:"required"`
 }
