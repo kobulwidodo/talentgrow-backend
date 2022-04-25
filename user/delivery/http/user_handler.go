e9c5d167b4ef5776a77df5b100b805a6a0b8b7f6
kobulwidodo <fadhilhan01@gmail.com>
2022-04-25 14:48:40 +0700

    feat: add new endpoint for get profile

--- user/delivery/http/user_handler.go
+++ user/delivery/http/user_handler.go
@@ -1,9 +1,10 @@
 package http
 
 import (
-	"talentgrow-backend/domain"
-	"talentgrow-backend/utils"
 	"net/http"
+	"talentgrow-backend/domain"
+	"talentgrow-backend/middleware"
+	"talentgrow-backend/utils"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,38 +13,50 @@ type UserHandler struct {
 	UserUseCase domain.UserUseCase
 }
 
-func NewUserHandler(r *gin.RouterGroup, uu domain.UserUseCase) {
+func NewUserHandler(r *gin.RouterGroup, uu domain.UserUseCase, jwtMiddleware gin.HandlerFunc) {
 	handler := &UserHandler{UserUseCase: uu}
 	api := r.Group("/auth")
 	{
 		api.POST("/signup", handler.SignUp)
 		api.POST("/signin", handler.SignIn)
 	}
+	r.GET("/me", jwtMiddleware, handler.GetMe)
 }
 
-func (u *UserHandler) SignUp(c *gin.Context) {
+func (h *UserHandler) SignUp(c *gin.Context) {
 	input := new(domain.UserSignUp)
 	if err := c.ShouldBindJSON(input); err != nil {
 		c.JSON(http.StatusBadRequest, utils.NewFailResponse(err.Error()))
 		return
 	}
-	if err := u.UserUseCase.SignUp(input); err != nil {
+	if err := h.UserUseCase.SignUp(input); err != nil {
 		c.JSON(http.StatusInternalServerError, utils.NewFailResponse(err.Error()))
 		return
 	}
 	c.JSON(http.StatusCreated, utils.NewSuccessResponse("user successfully registered", nil))
 }
 
-func (u *UserHandler) SignIn(c *gin.Context) {
+func (h *UserHandler) SignIn(c *gin.Context) {
 	input := new(domain.UserSignIn)
 	if err := c.ShouldBindJSON(input); err != nil {
 		c.JSON(http.StatusBadRequest, utils.NewFailResponse(err.Error()))
 		return
 	}
-	token, err := u.UserUseCase.SignIn(input)
+	token, err := h.UserUseCase.SignIn(input)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, utils.NewFailResponse(err.Error()))
 		return
 	}
 	c.JSON(http.StatusOK, utils.NewSuccessResponse("successfully login", map[string]string{"token": token}))
+}
+
+func (h *UserHandler) GetMe(c *gin.Context) {
+	email := c.MustGet("auth").(middleware.CustomClaim).Email
+	user, err := h.UserUseCase.GetMe(email)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, utils.NewFailResponse(err.Error()))
+		return
+	}
+	c.JSON(http.StatusOK, utils.NewSuccessResponse("successfully get me", user))
+	return
 }
