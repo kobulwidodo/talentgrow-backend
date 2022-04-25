51d5e122833c834338b51b714a886f6adbadc0d4
kobulwidodo <fadhilhan01@gmail.com>
2022-04-25 14:47:01 +0700

    feat: add some new repository method

--- internship_applicant/repository/postgresql/internship_applicant_postgresql.go
+++ internship_applicant/repository/postgresql/internship_applicant_postgresql.go
@@ -14,8 +14,31 @@ func NewInternshipApplicantPostgresRepository(db *gorm.DB) domain.InternshipAppl
 	return &InternshipApplicantPostgresRepository{db: db}
 }
 
-func (r *InternshipApplicantPostgresRepository) Create(internshipApplicant domain.InternshipApplicant) error {
+func (r *InternshipApplicantPostgresRepository) Create(internshipApplicant domain.InternshipApplicant) (uint, error) {
 	if err := r.db.Create(&internshipApplicant).Error; err != nil {
+		return 0, err
+	}
+	return internshipApplicant.ID, nil
+}
+
+func (r *InternshipApplicantPostgresRepository) FindOne(id uint) (domain.InternshipApplicant, error) {
+	var data domain.InternshipApplicant
+	if err := r.db.Where("id = ?", id).First(&data).Error; err != nil {
+		return data, err
+	}
+	return data, nil
+}
+
+func (r *InternshipApplicantPostgresRepository) FindByUserIdAndInternId(userId uint, internshipId uint) (domain.InternshipApplicant, error) {
+	var data domain.InternshipApplicant
+	if err := r.db.Where("user_id = ? AND internship_id = ?", userId, internshipId).Find(&data).Error; err != nil {
+		return data, err
+	}
+	return data, nil
+}
+
+func (r *InternshipApplicantPostgresRepository) UpdateCv(data domain.InternshipApplicant) error {
+	if err := r.db.Save(&data).Error; err != nil {
 		return err
 	}
 	return nil
