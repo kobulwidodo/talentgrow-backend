32fca92687426d2f43e71676f68a00c364930d0d
kobulwidodo <fadhilhan01@gmail.com>
2022-04-25 14:47:25 +0700

    feat: add some new usecase method

--- internship_applicant/usecase/internship_applicant_usecase.go
+++ internship_applicant/usecase/internship_applicant_usecase.go
@@ -1,6 +1,9 @@
 package usecase
 
-import "talentgrow-backend/domain"
+import (
+	"errors"
+	"talentgrow-backend/domain"
+)
 
 type InternshipAppilcantUseCase struct {
 	internshipApplicantPostgresRepository domain.InternshipApplicantPostgresRepository
@@ -11,10 +14,17 @@ func NewInternshipApplicantUseCase(iapr domain.InternshipApplicantPostgresReposi
 	return &InternshipAppilcantUseCase{internshipApplicantPostgresRepository: iapr, internshipRepository: ir}
 }
 
-func (u *InternshipAppilcantUseCase) Apply(input *domain.ApplyInternship) error {
-	_, err := u.internshipRepository.FindOne(input.InternshipId)
+func (u *InternshipAppilcantUseCase) Apply(input *domain.ApplyInternship) (uint, error) {
+	existData, err := u.internshipApplicantPostgresRepository.FindByUserIdAndInternId(input.UserId, input.InternshipId)
 	if err != nil {
-		return err
+		return 0, err
+	}
+	if existData.ID != 0 {
+		return 0, errors.New("already registered")
+	}
+	_, err = u.internshipRepository.FindOne(input.InternshipId)
+	if err != nil {
+		return 0, err
 	}
 	internshipApplicant := domain.InternshipApplicant{
 		UserId:       input.UserId,
@@ -24,7 +34,45 @@ func (u *InternshipAppilcantUseCase) Apply(input *domain.ApplyInternship) error 
 		CvLink:       input.CvLink,
 		InternshipId: input.InternshipId,
 	}
-	if err := u.internshipApplicantPostgresRepository.Create(internshipApplicant); err != nil {
+	id, err := u.internshipApplicantPostgresRepository.Create(internshipApplicant)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func (u *InternshipAppilcantUseCase) FindOne(input *domain.FindApplicant) (domain.InternshipApplicant, error) {
+	data, err := u.internshipApplicantPostgresRepository.FindOne(input.Id)
+	if err != nil {
+		return data, err
+	}
+	return data, nil
+}
+
+func (u *InternshipAppilcantUseCase) CheckIsRegistered(userId uint, internshipId uint) (domain.CheckRegisteredResponse, error) {
+	data, err := u.internshipApplicantPostgresRepository.FindByUserIdAndInternId(userId, internshipId)
+	if err != nil {
+		return domain.CheckRegisteredResponse{}, err
+	}
+	res := domain.CheckRegisteredResponse{
+		Age: data.Age,
+		College: data.College,
+		Major: data.Major,
+		IsRegistered: false,
+	}
+	if data.ID != 0 {
+		res.IsRegistered = true
+	}
+	return res, nil
+}
+
+func (u *InternshipAppilcantUseCase) UploadCv(path string, id uint) error {
+	data, err := u.internshipApplicantPostgresRepository.FindOne(id)
+	if err != nil {
+		return err
+	}
+	data.CvLink = path
+	if err := u.internshipApplicantPostgresRepository.UpdateCv(data); err != nil {
 		return err
 	}
 	return nil
